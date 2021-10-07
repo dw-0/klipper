605e9b8b1a1069684d7d64eaf6de5638f894df63
th33xitus <th33xitus@googlemail.com>
2021-10-07 13:50:37 +0200

    scripts: update klipper-uninstall.sh to remove systemd service

--- scripts/klipper-uninstall.sh
+++ scripts/klipper-uninstall.sh
@@ -3,17 +3,21 @@
 
 # Stop Klipper Service
 echo "#### Stopping Klipper Service.."
-sudo service klipper stop
+sudo systemctl stop klipper
 
 # Remove Klipper from Startup
 echo
 echo "#### Removing Klipper from Startup.."
 sudo update-rc.d -f klipper remove
+sudo systemctl disable klipper
 
 # Remove Klipper from Services
 echo
 echo "#### Removing Klipper Service.."
 sudo rm -f /etc/init.d/klipper /etc/default/klipper
+sudo rm -f /etc/systemd/system/klipper.service
+sudo rm -f /tmp/klippy_uds
+sudo rm -f /tmp/printer
 
 # Notify user of method to remove Klipper source code
 echo
