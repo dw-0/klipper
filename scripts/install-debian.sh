276cd5df24dd98bf8400e63aa23d0f5990a72c94
th33xitus <th33xitus@googlemail.com>
2021-10-07 13:51:25 +0200

    scripts: update install-debian.sh
    Update install-debian.sh to use Python 3 and install Klipper as systemd service

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -2,16 +2,27 @@
 # This script installs Klipper on an debian
 #
 
-PYTHONDIR="${HOME}/klippy-env"
+# Force script to exit if an error occurs
+set -e
+
+# Find SRCDIR from the pathname of this script
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+
+# Default file paths
 SYSTEMDDIR="/etc/systemd/system"
-KLIPPER_USER=$USER
-KLIPPER_GROUP=$KLIPPER_USER
+PYTHONDIR="${HOME}/klippy-env"
+PYTHONEXEC="${PYTHONDIR}/bin/python"
+KLIPPER_PATH="${SRCDIR}/klippy/klippy.py"
+CONFIG_PATH="${HOME}/klipper_config/printer.cfg"
+LOG_PATH="${HOME}/klipper_logs/klippy.log"
+PRINTER_PATH="/tmp/printer"
+UDS_PATH="/tmp/klippy_uds"
 
 # Step 1: Install system packages
 install_packages()
 {
     # Packages for python cffi
-    PKGLIST="virtualenv python-dev libffi-dev build-essential"
+    PKGLIST="python3-virtualenv python3-dev libffi-dev build-essential"
     # kconfig requirements
     PKGLIST="${PKGLIST} libncurses-dev"
     # hub-ctrl
@@ -24,7 +35,7 @@ install_packages()
 
     # Update system package info
     report_status "Running apt-get update..."
-    sudo apt-get update
+    sudo apt-get update --allow-releaseinfo-change
 
     # Install desired packages
     report_status "Installing packages..."
@@ -37,7 +48,7 @@ create_virtualenv()
     report_status "Updating python virtual environment..."
 
     # Create virtualenv if it doesn't already exist
-    [ ! -d ${PYTHONDIR} ] && virtualenv -p python2 ${PYTHONDIR}
+    [ ! -d ${PYTHONDIR} ] && virtualenv -p python3 ${PYTHONDIR}
 
     # Install/update dependencies
     ${PYTHONDIR}/bin/pip install -r ${SRCDIR}/scripts/klippy-requirements.txt
@@ -47,34 +58,47 @@ create_virtualenv()
 install_script()
 {
 # Create systemd service file
-    KLIPPER_LOG=/tmp/klippy.log
     report_status "Installing system start script..."
+
+    [ ! -d "${HOME}/klipper_logs" ] && mkdir "${HOME}/klipper_logs"
+    [ ! -d "${HOME}/klipper_config" ] && mkdir "${HOME}/klipper_config"
+
     sudo /bin/sh -c "cat > $SYSTEMDDIR/klipper.service" << EOF
 #Systemd service file for klipper
 [Unit]
-Description=Starts klipper on startup
+Description=Starts Klipper and provides a Unix Domain Socket API
+Documentation=https://www.klipper3d.org/
+Before=moonraker.service
 After=network.target
+Wants=udev.target
 
 [Install]
 WantedBy=multi-user.target
 
 [Service]
+Environment=KLIPPER=${KLIPPER_PATH}
+Environment=KLIPPER_CONFIG=${CONFIG_PATH}
+Environment=KLIPPER_LOGS=${LOG_PATH}
+Environment=KLIPPER_PRINTER=${PRINTER_PATH}
+Environment=KLIPPER_SOCKET=${UDS_PATH}
+
 Type=simple
-User=$KLIPPER_USER
-RemainAfterExit=yes
-ExecStart=${PYTHONDIR}/bin/python ${SRCDIR}/klippy/klippy.py ${HOME}/printer.cfg -l ${KLIPPER_LOG}
+User=$USER
+ExecStart=${PYTHONEXEC} \${KLIPPER} \${KLIPPER_CONFIG} -l \${KLIPPER_LOGS} -I \${KLIPPER_PRINTER} -a \${KLIPPER_SOCKET}
+
 Restart=always
 RestartSec=10
 EOF
 # Use systemctl to enable the klipper systemd service script
     sudo systemctl enable klipper.service
+    sudo systemctl daemon-reload
 }
 
 # Step 4: Start host software
 start_software()
 {
     report_status "Launching Klipper host software..."
-    sudo systemctl start klipper
+    sudo systemctl start klipper.service
 }
 
 # Helper functions
@@ -90,12 +114,6 @@ verify_ready()
         exit -1
     fi
 }
-
-# Force script to exit if an error occurs
-set -e
-
-# Find SRCDIR from the pathname of this script
-SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
 # Run installation steps defined above
 verify_ready
